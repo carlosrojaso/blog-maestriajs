97e9573f903066f588b3220abc7b09e9392963a1
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-05-30 17:13:55 -0500

    SEO stuff y programacion orientada al IF :p

--- sw.js
+++ sw.js
@@ -6,29 +6,20 @@ var urlsToCache = [
   '/css/main.css'
 ];
 {% for file in site.static_files %}
-    {% if file.path contains '/js' or file.path contains '/css' or file.path contains '/images' %}
-    urlsToCache.push("{{ file.path }}")
+    {% if file.path contains '/js' or file.path contains '/images' %}
+    urlsToCache.push("{{ file.path }}");
     {% endif %}
 {% endfor %}
-// Cache assets
-// Removed assets/posts because I only want assets from the most recent posts getting cached
-
-// {% for file in site.static_files %}
-//     {% if file.extname == '.js' or file.extname == '.css' or file.path contains '/images' %}
-//     urlsToCache.push("{{ file.path }}")
-//     {% endif %}
-// {% endfor %}
-
 
 // Cache posts
-// {% for post in site.posts %}
-//   urlsToCache.push("{{ post.url }}")
-// {% endfor %}
+{% for post in site.posts limit:5 %}
+   urlsToCache.push("{{ post.url }}");
+{% endfor %}
 
 // Cache pages
-// {% for page in site.html_pages %}
-//   urlsToCache.push("{{ page.url }}")
-// {% endfor %}
+{% for page in site.html_pages %}
+   urlsToCache.push("{{ page.url }}");
+{% endfor %}
 
 var CACHE_NAME = 'ion-book-cache-v1';
 
@@ -39,16 +30,39 @@ self.addEventListener('install', function(event) {
       .then(function(cache) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
-      })
+      }).catch(function(e) {
+      // Fallback to the offline page if not available in the cache.
+      console.log("error: " + e);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-    if (event.request.mode === 'navigate') {
-      event.respondWith(fetch('/pwa'));
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    }).catch(function() {
+      // Fallback to the offline page if not available in the cache.
+      return caches.match('/404.html');
+    })
+  );
+});
 
-      // Immediately start downloading the actual resource.
-      fetch(event.request.url);
-    }
-
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return fetch(event.request).then(function(response) {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch(function() {
+      // Fallback to the offline page if not available in the cache.
+      return caches.match('/404.html');
+    })
+  );
 });
