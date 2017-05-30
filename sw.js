804f8e7f246b1bc08a5eb6b13fbc722ddb9c5090
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-05-30 11:39:58 -0500

    fixing styles + service worker fixes

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
 ---
 layout: null
 ---
-var urlsToCache = [];
-
+var urlsToCache = [
+  '/',
+  '/css/main.css'
+];
 {% for file in site.static_files %}
     {% if file.path contains '/js' or file.path contains '/css' or file.path contains '/images' %}
     urlsToCache.push("{{ file.path }}")
@@ -10,23 +12,23 @@ var urlsToCache = [];
 {% endfor %}
 // Cache assets
 // Removed assets/posts because I only want assets from the most recent posts getting cached
-/*
-{% for file in site.static_files %}
-    {% if file.extname == '.js' or file.extname == '.css' or file.path contains '/images' %}
-    urlsToCache.push("{{ file.path }}")
-    {% endif %}
-{% endfor %}
-*/
+
+// {% for file in site.static_files %}
+//     {% if file.extname == '.js' or file.extname == '.css' or file.path contains '/images' %}
+//     urlsToCache.push("{{ file.path }}")
+//     {% endif %}
+// {% endfor %}
+
 
 // Cache posts
-/*{% for post in site.posts %}
-  urlsToCache.push("{{ post.url }}")
-{% endfor %}*/
+// {% for post in site.posts %}
+//   urlsToCache.push("{{ post.url }}")
+// {% endfor %}
 
 // Cache pages
-/*{% for page in site.html_pages %}
-  urlsToCache.push("{{ page.url }}")
-{% endfor %}*/
+// {% for page in site.html_pages %}
+//   urlsToCache.push("{{ page.url }}")
+// {% endfor %}
 
 var CACHE_NAME = 'ion-book-cache-v1';
 
@@ -41,32 +43,12 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.match(event.request).then(function(response) {
-        return response || fetch(event.request).then(function(response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    }).catch(function() {
-      // Fallback to the offline page if not available in the cache.
-      return caches.match('/404.html');
-    })
-  );
-});
+self.addEventListener('fetch', event => {
+    if (event.request.mode === 'navigate') {
+      event.respondWith(fetch('/pwa'));
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return fetch(event.request).then(function(response) {
-        cache.put(event.request, response.clone());
-        return response;
-      });
-    }).catch(function() {
-      // Fallback to the offline page if not available in the cache.
-      return caches.match('/offline.html');
-    })
-  );
+      // Immediately start downloading the actual resource.
+      fetch(event.request.url);
+    }
+
 });
