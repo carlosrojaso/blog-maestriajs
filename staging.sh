2ff8563f0a069ed087f31f99d7a428c97f036f94
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-10-30 11:26:12 -0500

    Migrando ...

--- staging.sh
+++ staging.sh
@@ -1,1 +1,8 @@
-git push -f origin master:gh-pages
+#!/bin/sh
+
+echo 'Comenzar Build'
+jekyll build --config _config.yml
+echo 'Agregando Manigest'
+cp manifest.json _site/manifest.json
+echo 'Comenzar Deploy'
+firebase deploy --only hosting -P staging
